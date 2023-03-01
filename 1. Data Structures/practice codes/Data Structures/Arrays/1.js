6e985ec42681b5f078f17c230f91845880ea33cc
anishbasnet_ab <anishbasnet027@gmail.com>
2023-03-01 12:00:52 +0545

    Array practice code

---
+++ 1. Data Structures/practice codes/Data Structures/Arrays/1.js
@@ -0,0 +1,32 @@
+// 1. Write a JavaScript function to check whether an `input` is an array or not. Go to the editor
+// Test Data :
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
+// false
+// true
+
+
+//my  solution
+function checkArray(arrayInp){
+    if(typeof arrayInp == "object"){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//easy solution
+function checkArray(arrayInp){
+    console.log(Array);
+   return Array.isArray(arrayInp);
+}
+
+
+console.log(checkArray([1, 2, 4, 0]));
+console.log(checkArray([1, 2, 4, 0]));
+console.log(checkArray(1));
+console.log(checkArray('w3resource'));
+console.log(checkArray([1, 2, 4, 0]));
+
+
